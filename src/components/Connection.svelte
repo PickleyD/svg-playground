6319243648f12e597b6c664cdab87f2d54630866
Matt Durkin <m.durkin92@gmail.com>
2023-07-15 21:48:22 +0100

    Add SVG with two circles and Connection component between them

---
+++ src/components/Connection.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+	type Vector2 = [x: number, y: number];
+
+	const dist = ([x1, y1]: Vector2, [x2, y2]: Vector2) => {
+		return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5;
+	};
+
+	const angle = ([x1, y1]: Vector2, [x2, y2]: Vector2) => {
+		return Math.atan2(y1 - y2, x1 - x2);
+	};
+
+	const getVector = ([cx, cy]: Vector2, a: number, r: number): Vector2 => {
+		return [cx + r * Math.cos(a), cy + r * Math.sin(a)];
+	};
+
+	const metaballToPath = (
+		p1: Vector2,
+		p2: Vector2,
+		p3: Vector2,
+		p4: Vector2,
+		h1: Vector2,
+		h2: Vector2,
+		h3: Vector2,
+		h4: Vector2,
+		escaped: boolean,
+		r1: number,
+		r2: number
+	) => {
+		return [
+			'M',
+			p1,
+			'C',
+			h1,
+			h3,
+			p3,
+			'A',
+			r2,
+			r2,
+			0,
+			escaped ? 1 : 0,
+			0,
+			p4,
+			'C',
+			h4,
+			h2,
+			p2,
+			'A',
+			r1,
+			r1,
+			0,
+			escaped ? 1 : 0,
+			0,
+			p1
+		].join(' ');
+	};
+
+	export let v = 0.5;
+	export let handleSize = 2.5;
+	export let radius1 = 30;
+	export let radius2 = 30;
+	export let center1: Vector2;
+	export let center2: Vector2;	
+    export let fill = '#000';
+	export let stroke = '#faa';
+	const d = dist(center1, center2);
+	const maxSpread = Math.acos((radius1 - radius2) / d);
+	const angleBetweenCenters = angle(center2, center1);
+
+	const u1 =
+		d < radius1 + radius2
+			? Math.acos((radius1 * radius1 + d * d - radius2 * radius2) / (2 * radius1 * d))
+			: 0;
+
+	const u2 =
+		d < radius1 + radius2
+			? Math.acos((radius2 * radius2 + d * d - radius1 * radius1) / (2 * radius2 * d))
+			: 0;
+
+	const angle1 = angleBetweenCenters + u1 + (maxSpread - u1) * v;
+	const angle2 = angleBetweenCenters - (u1 + (maxSpread - u1) * v);
+	const angle3 = angleBetweenCenters + Math.PI - u2 - (Math.PI - u2 - maxSpread) * v;
+	const angle4 = angleBetweenCenters - (Math.PI - u2 - (Math.PI - u2 - maxSpread) * v);
+
+	// Points
+	const p1 = getVector(center1, angle1, radius1);
+	const p2 = getVector(center1, angle2, radius1);
+	const p3 = getVector(center2, angle3, radius2);
+	const p4 = getVector(center2, angle4, radius2);
+
+	const totalRadius = radius1 + radius2;
+	// Handle length scaling factor
+	const d2 = Math.min(v * handleSize, dist(p1, p3) / totalRadius);
+	// Handle lengths
+	const r1 = radius1 * d2;
+	const r2 = radius2 * d2;
+
+	const HALF_PI = Math.PI / 2;
+
+	const h1 = getVector(p1, angle1 - HALF_PI, r1);
+	const h2 = getVector(p2, angle2 + HALF_PI, r1);
+	const h3 = getVector(p3, angle3 + HALF_PI, r2);
+	const h4 = getVector(p4, angle4 - HALF_PI, r2);
+
+	const path = metaballToPath(p1, p2, p3, p4, h1, h2, h3, h4, d > radius1, radius1, radius2);
+</script>
+
+<path d={path} {fill} {stroke}/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Connection from "../components/Connection.svelte"
+
+	type Vector2 = [x: number, y: number];
+
+	const dist = ([x1, y1]: Vector2, [x2, y2]: Vector2) => {
+		return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5;
+	};
+
+	const v = 0.5;
+	const radius1 = 30;
+	const radius2 = 30;
+	const center1: Vector2 = [230, 230];
+	const center2: Vector2 = [330, 330];
+    const d = dist(center1, center2)
+
+    const fill="#000"
+    const stroke="#faa"
+</script>
+
+<svg class="fixed top-0 left-0 h-full w-full" viewBox="0 0 1000 1000">
+	<rect width="100%" height="100%" fill="#fff" />
+	<g stroke-width="8">
+		<circle
+			id="circle-1"
+			cx={center1[0]}
+			cy={center1[1]}
+			r={radius1}
+			{fill}
+			{stroke}
+		/>
+
+		<circle
+			id="circle-2"
+			cx={center2[0]}
+			cy={center2[1]}
+			r={radius2}
+			{fill}
+			{stroke}
+		/>
+
+		<Connection center1={center1} center2={center2} fill='black' stroke='black' />
+	</g>
+</svg>
